8d790fb6d5159166a8d2c01988e5cfddbd74da0a
ADMIN <osmyorkin@gmail.com>
2020-06-06 14:49:33 +0300

    Stage 4/4: Bye, completed tasks

--- ToDo_list.py
+++ ToDo_list.py
@@ -1,6 +1,6 @@
 # JetBrains Academy/Python Developer
 # Project: To-Do List
-# Work on project. Stage 3/4: Deadlines are scary
+# Work on project. Stage 4/4: Bye, completed tasks
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -39,7 +39,9 @@ class ToDo:
         print('1) Today\'s tasks')
         print('2) Week\'s tasks')
         print('3) All tasks')
-        print('4) Add task')
+        print('4) Missed tasks')
+        print('5) Add task')
+        print('6) Delete task')
         print('0) Exit')
         self.menu_choice = input()
 
@@ -58,7 +60,8 @@ class ToDo:
     def show_weeks_tasks(self):
         """Outputs all tasks for next seven days"""
         for day in [datetime.today() + timedelta(days=i) for i in range(7)]:
-            tasks = self.session.query(self.Table).filter(self.Table.deadline == day.strftime('%Y-%m-%d')).all()
+            tasks = self.session.query(self.Table).filter(self.Table.deadline == day.strftime('%Y-%m-%d')).\
+                order_by(self.Table.deadline).all()
             print(f'{day.strftime("%A")} {day.strftime("%d %b")}:')
             if tasks:
                 for n, task in enumerate(tasks, 1):
@@ -69,13 +72,25 @@ class ToDo:
 
     def show_all_tasks(self):
         """Shows all tasks from the database"""
-        tasks = self.session.query(self.Table).all()
+        tasks = self.session.query(self.Table).order_by(self.Table.deadline).all()
         print('All tasks:')
         if tasks:
-            for task in tasks:
-                print(f'{task}. {task.deadline.strftime("%d %b")}')
+            for n, task in enumerate(tasks, 1):
+                print(f'{n}. {task.task}. {task.deadline.strftime("%d %b")}')
         else:
             print('Nothing to do!')
+        print()
+
+    def show_missed_tasks(self):
+        """Shows all missed tasks from the database"""
+        tasks = self.session.query(self.Table).filter(self.Table.deadline < datetime.today().strftime('%Y-%m-%d')).\
+            order_by(self.Table.deadline).all()
+        print('Missed tasks:')
+        if tasks:
+            for n, task in enumerate(tasks, 1):
+                print(f'{n}. {task.task}. {task.deadline.strftime("%d %b")}')
+        else:
+            print('Nothing is missed!')
         print()
 
     def add_task(self):
@@ -83,10 +98,22 @@ class ToDo:
         print('Enter task')
         text_task = input()
         print('Enter deadline')
-        new_task = self.Table(task=text_task, deadline=datetime.strptime(input(), '%Y-%m-%d'))  # strftime('%m-%d-%Y')
+        new_task = self.Table(task=text_task, deadline=datetime.strptime(input(), '%Y-%m-%d'))
         self.session.add(new_task)
         self.session.commit()
         print('The task has been added!')
+        print()
+
+    def delete_task(self):
+        tasks = self.session.query(self.Table).order_by(self.Table.deadline).all()
+        if tasks:
+            print('Chose the number of the task you want to delete:')
+            for n, task in enumerate(tasks, 1):
+                print(f'{n}. {task.task}. {task.deadline.strftime("%d %b")}')
+            self.session.query(self.Table).filter(self.Table.id == tasks[int(input())-1].id).delete()
+            self.session.commit()
+        else:
+            print('Nothing to delete!')
         print()
 
     def run(self):
@@ -100,7 +127,11 @@ class ToDo:
             elif self.menu_choice == '3':
                 self.show_all_tasks()
             elif self.menu_choice == '4':
+                self.show_missed_tasks()
+            elif self.menu_choice == '5':
                 self.add_task()
+            elif self.menu_choice == '6':
+                self.delete_task()
             else:
                 print('Bye!')
                 break
